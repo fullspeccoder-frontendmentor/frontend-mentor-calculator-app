f86058c813e20941186c3242a34196ca13888d8d
Jacob Wilson <jcbwlsn04@gmail.com>
2023-12-26 14:01:14 -0600

    Styled up the calculator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const KEYS = [
+  "7",
+  "8",
+  "9",
+  "DEL",
+  "4",
+  "5",
+  "6",
+  "+",
+  "1",
+  "2",
+  "3",
+  "-",
+  ".",
+  "0",
+  "/",
+  "x",
+  "RESET",
+  "=",
+];
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("ThemeOne");
+  const [number, setNumber] = useState(0);
+
+  const handleToggleTheme = (theme) => {
+    const availableThemes = ["ThemeOne", "ThemeTwo", "ThemeThree"];
+    if (!availableThemes.includes(theme)) return alert("Theme Not Available");
+    setCurrentTheme(theme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${currentTheme}`}>
+      <Header>
+        <h1>calc</h1>
+        <ThemeToggler onToggleTheme={handleToggleTheme} />
+      </Header>
+      <DisplayArea number={number} />
+      <Keys />
     </div>
   );
 }
+
+const Header = ({ children }) => {
+  return <header className="Header">{children}</header>;
+};
+
+const ThemeToggler = ({ onToggleTheme }) => {
+  return (
+    <aside className="ThemeToggler">
+      <span>THEME</span>
+      <div className="theme-numbers">
+        <span>
+          <span onClick={() => onToggleTheme("ThemeOne")}>1</span>
+          <span onClick={() => onToggleTheme("ThemeTwo")}>2</span>
+          <span onClick={() => onToggleTheme("ThemeThree")}>3</span>
+        </span>
+        <div className="slider">
+          <span className="slider-btn"></span>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+const DisplayArea = ({ number }) => {
+  return <section className="DisplayArea">{number}</section>;
+};
+
+const Keys = () => {
+  const [keyPressed, setKeyPressed] = useState("");
+
+  const handleClick = (e) => {
+    setKeyPressed(e.target.innerHTML);
+  };
+
+  return (
+    <main className="Keys">
+      {KEYS.map((keyChar, ind) => (
+        <button key={ind} onClick={handleClick}>
+          {keyChar}
+        </button>
+      ))}
+    </main>
+  );
+};
 
 export default App;
